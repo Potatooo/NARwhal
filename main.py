a209411a39e3cc0fe45e2749125bfe5b558aa7f5
Paweł Bożek <pawel.jan.bozek@gmail.com>
2017-12-29 14:41:50 +0100

    Add files via upload
    Checking status of database websites.

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from bs4 import BeautifulSoup
 import threading
 import time
 import urllib3
+from enum import Enum
+from collections import defaultdict
 
 __author__ = 'Pawel'
+
+class StatusCode(Enum):
+    UNKNOWN = 1
+    GOOD = 2
+    BAD = 3
 
 class NARwhalData:
     def __init__(self):
@@ -14,6 +21,7 @@ class NARwhalData:
         self.NAR_title = ""
         self.NAR_subtitle = ""
         self.NAR_href = ""
+        self.status = StatusCode.UNKNOWN
 
 class NARwhal:
     DOMAIN_LINK = "http://www.oxfordjournals.org"
@@ -38,6 +46,8 @@ class NARwhal:
         self._populate()
         self._visitSummaries()
 
+        self._visitDatabases()
+
     def _isVisited(self, url):
         return url in self.visited
 
@@ -58,6 +68,7 @@ class NARwhal:
                 if(len(self.summary_links) >= self.limit and self.limit >= 0):
                     break
 
+        print("Fetched links to subpages.")
         print("Time:", time.time() - start)
 
     def _extractData(self, url, pageText):
@@ -120,12 +131,48 @@ class NARwhal:
 
         for i in self.data:
             print(i.NAR_summary_url, i.NAR_title, i.NAR_subtitle, i.NAR_href)
+        print("Fetched links to databases.")
         print("Elapsed Time: %s" % (time.time() - start))
         print("Count:", self.count)
 
+    def _visitDatabases(self):
+        start = time.time()
+
+        countLock = threading.Lock()
+
+        def fetch_database(dbData):
+            triesLeft = self.retryCount
+            while(triesLeft > 0):
+                try:
+                    page = requests.get(dbData.NAR_href, timeout=self.singleRequestTimeout)
+                    if page.status_code >= 200 and page.status_code < 300:
+                        dbData.status = StatusCode.GOOD
+                    else:
+                        dbData.status = StatusCode.BAD
+                    break
+                except (ConnectionError, ConnectionResetError, urllib3.exceptions.ProtocolError, requests.exceptions.ConnectionError, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout, requests.exceptions.ChunkedEncodingError, requests.exceptions.InvalidSchema, requests.exceptions.ChunkedEncodingError, socket.timeout, http.client.IncompleteRead, requests.exceptions.ContentDecodingError) as e:
+                    pass
+                triesLeft -= 1
+                time.sleep(self.retrySleep)
+
+        threads = [threading.Thread(target=fetch_database, args=(db,)) for db in self.data]
+        [thread.start() for thread in threads]
+        [thread.join() for thread in threads]
+
+        print("Fetched databases information.")
+        print("Elapsed Time: %s" % (time.time() - start))
+
+        statusDict = defaultdict(int)
+        for db in self.data:
+            statusDict[db.status] += 1
+
+        print("STATUS:")
+        print("GOOD:   \t", statusDict[StatusCode.GOOD])
+        print("BAD:    \t", statusDict[StatusCode.BAD])
+        print("UNKNOWN:\t", statusDict[StatusCode.UNKNOWN])
 
 def main():
-    narv = NARwhal(retryCount=5, retrySleep=5, singleRequestTimeout=60, limit=21)
+    narv = NARwhal(retryCount=5, retrySleep=5, singleRequestTimeout=60, limit=25)
 
 main()
 
